9ebad97370deb0f2c4abd3fe36eb21c70d35a39b
sebin <programmer989@gmail.com>
2019-01-07 14:32:19 +0530

    changes

--- web/js/main.js
+++ web/js/main.js
@@ -1,25 +1,12 @@
 (function() {
 
     $(window).on('load', function() {
-        $('.gm-toggle-login').on('click', function() {
-            if ($(this).hasClass('active')) {
-
-                $('.gm-toggle-login').removeClass('active');
-                $('.gm-login-wrapper').removeClass('active');
-            } else {
-                $('.gm-toggle-login').addClass('active');
-                $('.gm-login-wrapper').addClass('active');
-            }
-        });
-        $('.gm-toggle-search').on('click', function() {
-            if ($(this).hasClass('active')) {
-
-                $('.gm-toggle-search,.gm-search-wrapper').removeClass('active');
-            } else {
-                $('.gm-toggle-search,.gm-search-wrapper').addClass('active');
-            }
-        });
-
+            $('[data-toggle="chat"]').on('click',function(){
+                var $this= $(this);
+                $('.ed-join,.ed-content').removeClass('active');
+                $this.addClass('active');
+                $('#'+$this.attr('target')).addClass('active');
+            })
 
     })
     
