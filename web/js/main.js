99fbddd3ac27e6d4b67399497abaaf0c09fbd473
sebin <programmer989@gmail.com>
2019-01-07 17:12:44 +0530

    html

--- web/js/main.js
+++ web/js/main.js
@@ -8,6 +8,22 @@
                 $('#'+$this.attr('target')).addClass('active');
             })
 
+            $('.ed-timer-blue').final_countdown({
+                'start': 1546679960,
+                'end': 1546906639,
+                'now': 1546874408,
+                selectors: {
+                    value_seconds: '.clock-seconds',
+                    canvas_seconds: 'canvas-seconds',
+                    value_minutes: '.clock-minutes',
+                    canvas_minutes: 'canvas-minutes',
+                    value_hours: '.clock-hours',
+                    canvas_hours: 'canvas-hours',
+                    value_days: '.clock-days',
+                    canvas_days: 'canvas-days'
+
+                }        
+            });
     })
     
 })();
