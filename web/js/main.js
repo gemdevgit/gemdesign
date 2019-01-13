a9e22a3f4af49cee2b323df3719d8e52776ba3ad
sebin <programmer989@gmail.com>
2019-01-13 13:24:56 +0530

    Modal

--- web/js/main.js
+++ web/js/main.js
@@ -21,6 +21,18 @@
                 dismissModal(target)
             })
 
+            $(document).mouseup(function (e) {
+                var container = $(".modal-dialog"); // YOUR CONTAINER SELECTOR
+
+                if (!container.is(e.target) // if the target of the click isn't the container...
+                    && container.has(e.target).length === 0) // ... nor a descendant of the container
+                {
+                    if(e.target.className == "modal-backdrop"){
+                        var target = $(e.target.parentElement).attr('id');
+                        dismissModal('#'+target)
+                    }
+                }
+            });
             function dismissModal(target){
                 $(target).removeClass('in').fadeOut(500);
             }
